93668ca5954cd93d245183e4b25a4019f547f0ea
unaguna <k.izumi.ysk@gmail.com>
2021-11-21 12:49:39 +0900

    ✅change interface

--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,20 @@ use chrono::{Date, Datelike, DateTime, NaiveDate, NaiveDateTime, Timelike, TimeZ
 
 pub struct LeapUtc {
     // うるう秒によってずれるタイミング (UTC)
-    datetime: NaiveDateTime,
+    datetime: DateTime<Utc>,
     // うるう秒による累積のずれ (TAI - UTC)
     diff_seconds: i32,
 }
 
-pub fn utc2tai(datetime: DateTime<Utc>, leaps: Vec<LeapUtc>) -> NaiveDateTime {
+pub fn utc2tai(datetime: &DateTime<Utc>, leaps: &[LeapUtc]) -> NaiveDateTime {
     panic!()
 }
 
 #[test]
 fn it_works() {
     let utc = Utc.ymd(2017, 1, 2).and_hms(11, 22, 33);
-    let leaps = vec![LeapUtc { datetime: NaiveDate::from_ymd(2017, 1, 1).and_hms(0, 0, 0), diff_seconds: 37 }];
-    let tai = utc2tai(utc, leaps);
+    let leaps = vec![LeapUtc { datetime: Utc.ymd(2017, 1, 1).and_hms(0, 0, 0), diff_seconds: 37 }];
+    let tai = utc2tai(&utc, &leaps);
 
     assert_eq!(tai.year(), 2017);
     assert_eq!(tai.month(), 1);
