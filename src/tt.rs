4476dd566b8bd419ce40ca30b38a66b7b37f61bc
unaguna <k.izumi.ysk@gmail.com>
2021-11-21 23:32:18 +0900

    ✨convert TT to UTC

---
+++ src/tt.rs
@@ -0,0 +1,15 @@
+use crate::DT_FMT;
+use chrono::{Duration, NaiveDateTime};
+
+const D_TT_TAI_MS: i64 = 32184;
+
+pub fn tt2tai(datetime: &str) -> Result<String, String> {
+    NaiveDateTime::parse_from_str(datetime, DT_FMT)
+        .map_err(|err| err.to_string())
+        .map(|datetime| tt2tai_dt(&datetime))
+        .map(|utc| utc.format(DT_FMT).to_string())
+}
+
+fn tt2tai_dt(datetime: &NaiveDateTime) -> NaiveDateTime {
+    return datetime.clone() - Duration::milliseconds(D_TT_TAI_MS);
+}
