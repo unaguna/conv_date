ef70c39fa47a09002e3525bcad3f28ffabc27ec5
unaguna <k.izumi.ysk@gmail.com>
2021-11-23 18:16:00 +0900

    ✨Multiple input

--- src/tt2utc/main.rs
+++ src/tt2utc/main.rs
@@ -1,18 +1,18 @@
 use conv_date::{exe, tai2utc, tt2tai};
-use std::env;
 
 fn main() {
     // Analize the arguments
-    // TODO: error checking
-    let args: Vec<String> = env::args().collect();
-    let in_tt = &args[1];
+    let args = exe::Arguments::new("Converter from TT to UTC");
 
     // load leap list
     let leaps = exe::get_leaps_path()
         .and_then(|p| exe::load_leaps(&p))
         .unwrap();
-    // calc UTC
-    let utc = tt2tai(in_tt).and_then(|tai| tai2utc(&tai, &leaps)).unwrap();
 
-    println!("{}", utc)
+    // calc UTC
+    for in_tt in args.get_datetimes() {
+        let utc = tt2tai(in_tt).and_then(|tai| tai2utc(&tai, &leaps)).unwrap();
+
+        println!("{}", utc)
+    }
 }
--- src/utc2tt/main.rs
+++ src/utc2tt/main.rs
@@ -8,7 +8,8 @@ fn main() {
     let leaps = exe::get_leaps_path()
         .and_then(|p| exe::load_leaps(&p))
         .unwrap();
-    // calc UTC
+
+    // calc TT
     for in_utc in args.get_datetimes() {
         let tt = utc2tai(in_utc, &leaps)
             .and_then(|tai| tai2tt(&tai))
