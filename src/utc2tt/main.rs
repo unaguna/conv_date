3b56fa839dc2cb0b2d767635c7129056eb859a2d
unaguna <k.izumi.ysk@gmail.com>
2021-11-23 18:08:33 +0900

    ♻encapsulate analysis of arguments

--- src/utc2tt/main.rs
+++ src/utc2tt/main.rs
@@ -1,22 +1,15 @@
-use clap::{App, Arg};
 use conv_date::{exe, tai2tt, utc2tai};
 
 fn main() {
     // Analize the arguments
-    let app = App::new("utc2tt").arg(
-        Arg::with_name("datetime")
-            .help("datetime to convert")
-            .multiple(true)
-            .required(true),
-    );
-    let matches = app.get_matches();
+    let args = exe::Arguments::new("Converter from UTC to TT");
 
     // load leap list
     let leaps = exe::get_leaps_path()
         .and_then(|p| exe::load_leaps(&p))
         .unwrap();
     // calc UTC
-    for in_utc in matches.values_of("datetime").unwrap() {
+    for in_utc in args.get_datetimes() {
         let tt = utc2tai(in_utc, &leaps)
             .and_then(|tai| tai2tt(&tai))
             .unwrap();
