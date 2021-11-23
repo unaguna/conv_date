ae936c29891b72de5cc1cd7b1a27057c136fa37b
unaguna <k.izumi.ysk@gmail.com>
2021-11-23 17:19:09 +0900

    ✨Multiple input

--- src/utc2tt/main.rs
+++ src/utc2tt/main.rs
@@ -6,19 +6,21 @@ fn main() {
     let app = App::new("utc2tt").arg(
         Arg::with_name("datetime")
             .help("datetime to convert")
+            .multiple(true)
             .required(true),
     );
     let matches = app.get_matches();
-    let in_utc = matches.value_of("datetime").unwrap();
 
     // load leap list
     let leaps = exe::get_leaps_path()
         .and_then(|p| exe::load_leaps(&p))
         .unwrap();
     // calc UTC
-    let tt = utc2tai(in_utc, &leaps)
-        .and_then(|tai| tai2tt(&tai))
-        .unwrap();
+    for in_utc in matches.values_of("datetime").unwrap() {
+        let tt = utc2tai(in_utc, &leaps)
+            .and_then(|tai| tai2tt(&tai))
+            .unwrap();
 
-    println!("{}", tt)
+        println!("{}", tt)
+    }
 }
