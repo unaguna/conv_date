4399eee4cf32c43becc08f1f0cdae5342d0551fc
unaguna <k.izumi.ysk@gmail.com>
2021-12-05 20:18:07 +0900

    📝Make documents of binary crates

--- src/bin/tt2utc.rs
+++ src/bin/tt2utc.rs
@@ -1,7 +1,32 @@
+//! This binary crate converts datetimes
+//! from [TT](https://en.wikipedia.org/wiki/Terrestrial_Time)
+//! to [UTC](https://en.wikipedia.org/wiki/Coordinated_Universal_Time).
+//!
+//! For example:
+//! ```bash
+//! $ tt2utc 2017-01-01T00:01:09
+//! 2016-12-31T23:59:60.816
+//! ```
+//!
+//! As you can see from the above example, it takes leap seconds into account.
+//!
+//! In this execution, it assume that
+//! TT = [TAI](https://en.wikipedia.org/wiki/International_Atomic_Time) + 32.184.
+//!
+//! # Arguments
+//! See [utc2tt#Arguments](../utc2tt/index.html#arguments).
+//!
+//! # Options
+//! See [utc2tt#Options](../utc2tt/index.html#options).
+//!
+//! # Environment variables
+//! See [utc2tt#Environment variables](../utc2tt/index.html#environment-variables).
+
 extern crate convdate;
 use convdate::exe::tt2utc::main_inner;
 use std::{env, io};
 
+#[doc(hidden)]
 fn main() {
     let exit_code = main_inner(
         env::args(),
